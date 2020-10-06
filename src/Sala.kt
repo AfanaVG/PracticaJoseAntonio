8775d1510698867d7565e0f115055f9a2e8a60b2
José <afana500@gmail.com>
2020-10-06 13:37:14 +0200

    Creada la clase sala y sus subclases Magico, Accion, Habilidad

--- src/Sala.kt
+++ src/Sala.kt
@@ -1,2 +1,14 @@
-open class Sala {
+open class Sala(var tipo: String) {
+
+    class Magico (tipo: String ,var poder_maligno : Int) : Sala(tipo){
+
+    }
+
+    class Accion(tipo: String ,var flechas : Int,var enemigos : Int) : Sala(tipo){
+
+    }
+
+    class  Habilidad(tipo: String) : Sala(tipo){
+
+    }
 }
