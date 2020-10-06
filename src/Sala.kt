d193f21a45fd575ce180e55db793f60e809d1705
José <afana500@gmail.com>
2020-10-06 18:09:59 +0200

    Implementados los metodos completarMagico, completarAccion, completarHabilidad

--- src/Sala.kt
+++ src/Sala.kt
@@ -1,14 +1,31 @@
+import kotlin.random.Random
+
 open class Sala(var tipo: String) {
 
     class Magico (tipo: String ,var poder_maligno : Int) : Sala(tipo){
+
+        fun completarMagico(){
+            this.tipo = "magico"
+            this.poder_maligno = Random.nextInt(1,10)
+        }
 
     }
 
     class Accion(tipo: String ,var flechas : Int,var enemigos : Int) : Sala(tipo){
 
+        fun completarAccion(){
+            this.tipo = "accion"
+            this.flechas = Random.nextInt(1,10)
+            this.enemigos = Random.nextInt(1,10)
+        }
+
     }
 
     class  Habilidad(tipo: String) : Sala(tipo){
+
+        fun completarHabilidad(){
+            this.tipo = "magico"
+        }
 
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -7,6 +7,10 @@ fun main (args: Array<String>){
 
     var gandalf = Personaje.Mago("Gandalf",true,30)
     var legolas = Personaje.Elfo("Legolas",true,20)
+    var salaM : Sala.Magico;
+    var salaA : Sala.Accion;
+    var salaH : Sala.Habilidad;
+    var ranSala : Int;
 
     gandalf.recargarVara(Random.nextInt(1,29))
     gandalf.poderVara()
@@ -17,4 +21,21 @@ fun main (args: Array<String>){
     legolas.recargarFlechas(1)
     print("flechas "+legolas.carcaj)
 
+    for (num in 1..36){
+        ranSala = Random.nextInt(1,3)
+        if (ranSala==1){
+            salaM = Sala.Magico("",0)
+            salaM.completarMagico()
+        }else if (ranSala==2){
+            salaA = Sala.Accion("",0,0)
+            salaA.completarAccion()
+        }else if (ranSala==3){
+            salaH = Sala.Habilidad("")
+            salaH.completarHabilidad()
+        }
+
+    }
+
 }
+
+
